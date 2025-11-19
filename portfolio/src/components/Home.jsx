63edcd00142da7612948542f82c57a4c2142f2fb
matrakei <163850982+matrakei@users.noreply.github.com>
2025-11-19 17:34:40 -0300

    .

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom"
 import '../styles/home.css'
 import matias from '../assets/matias.png'
 
 const Home = () => {
+  const navigate = useNavigate()
+
   return (
     <section className="home">
       <div className="home-text">
         <h1>Soy <span>Matias.</span></h1>
         <h2>Estudio para ser <span>Ingeniero</span></h2>
+
         <div className="home-buttons">
-          <button>Conocerme</button>
-          <button>Contactarme</button>
+          <button onClick={() => navigate('/quien-soy')}>Conocerme</button>
+          <button onClick={() => navigate('/contacto')}>Contactarme</button>
         </div>
       </div>
 
--- portfolio/src/pages/Habilidades.jsx
+++ portfolio/src/pages/Habilidades.jsx
@@ -1,9 +1,55 @@
 import '../styles/habilidades.css'
+import chip from '../assets/chip.png'
+import team from '../assets/team.png'
+import translate from '../assets/translate.png'
 
-const About = () => {
+const Habilidades = () => {
   return (
-    true
+    <section className="habilidades">
+      <h1 className="hab-titulo">Mis habilidades</h1>
+
+      <ul className="hab-lista">
+        <li>Me sé adaptar al entorno de trabajo con facilidad, siempre tengo ganas de aprender y de hacer cosas.</li>
+        <li>Me dispongo a hacer cualquier tipo de trabajo.</li>
+        <li>A la hora de hacer un trabajo organizo a mi favor el entorno para poder resolver con mayor facilidad.</li>
+      </ul>
+
+      <div className="hab-cards">
+        {/* CARD 1 */}
+        <div className="hab-card">
+          <p className="hab-card-titulo">Conocimiento de computación</p>
+          <div className="hab-icono">
+            <img src={chip} alt="chip" />
+          </div>
+          <p className="hab-card-desc">
+            Tengo amplio conocimiento técnico sobre hardware de computadoras.
+          </p>
+        </div>
+
+        {/* CARD 2 */}
+        <div className="hab-card">
+          <p className="hab-card-titulo">Trabajo en equipo</p>
+          <div className="hab-icono">
+            <img src={team} alt="team" />
+          </div>
+          <p className="hab-card-desc">
+            Soy muy bueno para trabajar en equipo y también liderar uno.
+          </p>
+        </div>
+
+        {/* CARD 3 */}
+        <div className="hab-card">
+          <p className="hab-card-titulo">Buen nivel de inglés</p>
+          <div className="hab-icono">
+            <img src={translate} alt="translate" />
+          </div>
+          <p className="hab-card-desc">
+            Tengo muy buen nivel de inglés, me gradué con un nivel B2 (pre-advanced).
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default About
+export default Habilidades
